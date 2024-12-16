fa9666704aa688aae4f0e02cea4b21f66a2297d8
Heba Yasser <hebay99@gmail.com>
2024-12-16 23:43:00 +0200

    create home page

--- components/SignupWithGoogleBtn.tsx
+++ components/SignupWithGoogleBtn.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Button } from "./ui";
 import { Icon } from "@iconify/react";
 
-const SignupWithGoogleBtn = ({children}) => {
+const SignupWithGoogleBtn = ({ children }: { children: ReactNode }) => {
   return (
     <Button
       variant={"outline"}
